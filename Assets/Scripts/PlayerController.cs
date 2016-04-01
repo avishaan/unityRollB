3822fa715d8a0df594a6ab2b3805018528d4d12b
Avishaan <avishaan@codeHatcher.com>
2016-04-01 13:13:26 -0500

    use keyboard inputs to move the ball

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+	public float speed; // public var shows up in editor as a property
+	private Rigidbody rb;
+	void Start() {
+		rb = GetComponent<Rigidbody> ();
+	}
+	void FixedUpdate() {
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVerticle = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVerticle);
+
+		rb.AddForce (movement * speed);
+	}
+
+}
