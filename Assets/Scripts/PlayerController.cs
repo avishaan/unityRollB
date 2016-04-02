ed8f04ac6b4ec6fa71b7c1d3bbc246b64dd432dc
Avishaan <avishaan@codeHatcher.com>
2016-04-02 18:20:53 -0500

    count picked up objects and show when a win occurs

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,11 +1,19 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour {
 	public float speed; // public var shows up in editor as a property
+  public Text countText;
+  public Text winText;
+
+  private int count;
 	private Rigidbody rb;
 	void Start() {
 		rb = GetComponent<Rigidbody> ();
+    count = 0;
+    SetCountText ();
+    winText.text = "";
 	}
 	void FixedUpdate() {
 		float moveHorizontal = Input.GetAxis ("Horizontal");
@@ -19,6 +27,15 @@ public class PlayerController : MonoBehaviour {
   void OnTriggerEnter(Collider other) {
     if (other.gameObject.CompareTag("Pick Up")){
       other.gameObject.SetActive (false);
+      count = count + 1;
+      SetCountText();
+    }
+  }
+
+  void SetCountText() {
+    countText.text = "Count: " + count.ToString();
+    if (count >= 13) {
+      winText.text = "You Win !!!";
     }
   }
 
